982da24d3eb910ecb872fc54f48ade504fa72d78
shoobham84 <shubhamnayak392reborn@gmail.com>
2026-03-22 15:23:10 +0530

    feat: implement SIGINT and SIGTERM exit signals to kill child processes

--- lls_monitor.sh
+++ lls_monitor.sh
@@ -6,12 +6,20 @@ echo "Starting LLS monitor with CPU limit $CPU_LIMITl%"
 echo "PID: $$"
 echo "--------------------------------------------------"
 
-if [ $(pgrep -cf "lls_monitor.sh") -gt 1]; then
+if [ $(pgrep -cf "lls_monitor.sh") -gt 1 ]; then
     echo "Error: LLS Monitor already running in background. Check background jobs"
     echo exit 1
 fi
 
+cleanup() {
+    echo "Termination signal. Shutting down-------"
+    pkill -P $$
+    exit 0
+}
+
+trap cleanup SIGINT SIGTERM 
+
 while true; do
     echo "[$(date +'%H-%M-%S')] running diagnostics"
-    sleep 10
+    sleep 5 
 done
