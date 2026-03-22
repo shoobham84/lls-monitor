45fb8dcbb17fa915c62905afff9be9e440c8c1c2
shoobham84 <shubhamnayak392reborn@gmail.com>
2026-03-22 17:29:38 +0530

    feat: add system RAM monitoring using free and awk

--- lls_monitor.sh
+++ lls_monitor.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
 CPU_LIMIT=${MONITOR_LIMIT:-5.0}
+MEM_LIMIT=${MONITOR_MEM_LIMIT:-80.0}
 
-echo "Starting LLS monitor with CPU limit $CPU_LIMIT%"
+echo "Starting LLS monitor with CPU limit $CPU_LIMIT% and MEM limit $MEM_LIMIT%"
 echo "PID: $$"
 echo "--------------------------------------------------"
 
@@ -26,8 +27,19 @@ while true; do
     echo "[$(date +'%H-%M-%S')] running diagnostics"
 
     echo " -> Heavy Processes (>$CPU_LIMIT% CPU):"
+
     ps aux | awk -v limit="$CPU_LIMIT" '$3 > limit { print "    PID:", $2, "| CPU%:", $3, "| CMD:", $11 }'
     
+    free -m | awk -v limit="$MEM_LIMIT" 'NR==2 {
+        percent = $3/$2 * 100;
+        if (percent > limit) {
+            printf "HIGH System RAM: %.1f%%\n", percent
+        }
+        else {
+            printf "System RAM: %.1f%%\n", percent
+        }
+    }'
+
     echo "------------------------------------------------"
 
     sleep 5 
