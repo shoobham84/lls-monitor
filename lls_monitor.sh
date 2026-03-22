820d5fdf414b3c6eb22ecfbcc2753f5a7cb9d60d
shoobham84 <shubhamnayak392reborn@gmail.com>
2026-03-22 15:28:57 +0530

    fix(exit): fix bug where it echo'd exit status instead of outputting exit; refactor(interval): change sleep 10 to 5

--- lls_monitor.sh
+++ lls_monitor.sh
@@ -2,13 +2,13 @@
 
 CPU_LIMIT=${MONITOR_LIMIT:-5.0}
 
-echo "Starting LLS monitor with CPU limit $CPU_LIMITl%"
+echo "Starting LLS monitor with CPU limit $CPU_LIMIT%"
 echo "PID: $$"
 echo "--------------------------------------------------"
 
 if [ $(pgrep -cf "lls_monitor.sh") -gt 1 ]; then
     echo "Error: LLS Monitor already running in background. Check background jobs"
-    echo exit 1
+    exit 1
 fi
 
 cleanup() {
