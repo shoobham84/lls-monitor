a7458e357537c48df48fc653ad1af381bf3491f9
shoobham84 <shubhamnayak392reborn@gmail.com>
2026-03-22 15:09:59 +0530

    feat: add pgrep mutex to prevent multiple instances

--- lls_monitor.sh
+++ lls_monitor.sh
@@ -6,6 +6,11 @@ echo "Starting LLS monitor with CPU limit $CPU_LIMITl%"
 echo "PID: $$"
 echo "--------------------------------------------------"
 
+if [ $(pgrep -cf "lls_monitor.sh") -gt 1]; then
+    echo "Error: LLS Monitor already running in background. Check background jobs"
+    echo exit 1
+fi
+
 while true; do
     echo "[$(date +'%H-%M-%S')] running diagnostics"
     sleep 10
