dc60ba8512758d1b7181a2e135caaa7904e716b4
shoobham84 <shubhamnayak392reborn@gmail.com>
2026-03-22 15:52:28 +0530

    refactor: integrate mktemp for secure file handling

--- lls_monitor.sh
+++ lls_monitor.sh
@@ -7,6 +7,8 @@ echo "PID: $$"
 echo "--------------------------------------------------"
 
 if [ $(pgrep -cf "lls_monitor.sh") -gt 1 ]; then
+    SECURE_TMP=$(mktmp /tmp/lls_monitor.XXXXXX)
+    echo "Secure temp file allocated at: $SECURE_TMP"
     echo "Error: LLS Monitor already running in background. Check background jobs"
     exit 1
 fi
@@ -14,6 +16,9 @@ fi
 cleanup() {
     echo "Termination signal. Shutting down-------"
     pkill -P $$
+
+    rm -f "$SECURE_TMP"
+    echo "Cleaned up at $SECURE_TMP"
     exit 0
 }
 
